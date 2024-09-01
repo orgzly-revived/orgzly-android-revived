9d3c37bc85bd026a4fc042a0dc08be376f846389
Victor Andreasson <victor.andreasson@fripost.org>
2024-09-01 12:29:42 +0200

    Use a single I/O thread when deleting multiple notebooks
    This resolves issue #315.

--- app/src/main/java/com/orgzly/android/usecase/BookDelete.kt
+++ app/src/main/java/com/orgzly/android/usecase/BookDelete.kt
@@ -2,12 +2,12 @@ package com.orgzly.android.usecase
 
 import com.orgzly.android.data.DataRepository
 
-class BookDelete(val bookId: Long, val deleteLinked: Boolean) : UseCase() {
+class BookDelete(val bookIds: Set<Long>, val deleteLinked: Boolean) : UseCase() {
     override fun run(dataRepository: DataRepository): UseCaseResult {
-        val book = dataRepository.getBookView(bookId) ?: throw NotFound()
-
-        dataRepository.deleteBook(book, deleteLinked)
-
+        for (bookId in bookIds) {
+            val book = dataRepository.getBookView(bookId) ?: throw NotFound()
+            dataRepository.deleteBook(book, deleteLinked)
+        }
         return UseCaseResult(
                 modifiesLocalData = true,
                 triggersSync = SYNC_DATA_MODIFIED
