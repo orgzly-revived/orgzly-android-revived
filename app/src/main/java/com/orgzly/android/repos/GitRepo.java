d30691c898d049633dcc86392ced0be527ae7fcf
Florian Friesdorf <florian@ternaris.com>
2025-04-15 19:22:07 +0200

    Ignore hidden files in git repos

--- app/src/main/java/com/orgzly/android/repos/GitRepo.java
+++ app/src/main/java/com/orgzly/android/repos/GitRepo.java
@@ -267,7 +267,10 @@ public class GitRepo implements SyncRepo, TwoWaySyncRepo {
             public boolean include(TreeWalk walker) {
                 final FileMode mode = walk.getFileMode();
                 final boolean isDirectory = mode == FileMode.TREE;
+                final String name = walk.getNameString();
                 final String repoRelativePath = walk.getPathString();
+                if (name.startsWith("."))
+                    return false;
                 if (ignores.isIgnored(repoRelativePath, isDirectory) == IgnoreNode.MatchResult.IGNORED)
                     return false;
                 if (isDirectory)
