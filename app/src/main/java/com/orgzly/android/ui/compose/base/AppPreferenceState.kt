badcb6009f0aafe516b9d21605bd2ec2eedd1dce
Emily McLean <emilymcleandeveloper@gmail.com>
2026-01-12 14:22:59 +1100

    Listen to appPreference changes

---
+++ app/src/main/java/com/orgzly/android/ui/compose/base/AppPreferenceState.kt
@@ -0,0 +1,29 @@
+package com.orgzly.android.ui.compose.base
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.platform.LocalContext
+import androidx.preference.PreferenceManager
+
+@Composable
+fun <T> appPreference(value: (Context) -> T): State<T> {
+    val context = LocalContext.current
+
+    return produceState(
+        initialValue = value(context), context
+    ) {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, _ ->
+            this.value = value(context)
+        }
+
+        prefs.registerOnSharedPreferenceChangeListener(listener)
+
+        awaitDispose {
+            prefs.unregisterOnSharedPreferenceChangeListener(listener)
+        }
+    }
+}
