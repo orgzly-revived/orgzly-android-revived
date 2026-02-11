4c61f9aa500ff11b39dd68753e75e0f57a035b54
Aleksandar Terentić <aterentic@pm.me>
2026-02-11 23:19:21 +0100

    Move instrumented tags tests to JVM

--- app/src/test/java/com/orgzly/android/misc/FiletagsTest.kt
+++ app/src/test/java/com/orgzly/android/misc/FiletagsTest.kt
@@ -1,12 +1,65 @@
 package com.orgzly.android.misc
 
-import com.orgzly.android.OrgzlyTest
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.orgzly.android.LocalStorage
+import com.orgzly.android.NotesOrgExporter
+import com.orgzly.android.TestUtils
+import com.orgzly.android.data.DataRepository
+import com.orgzly.android.data.DbRepoBookRepository
+import com.orgzly.android.db.OrgzlyDatabase
 import com.orgzly.android.db.entity.toList
+import com.orgzly.android.prefs.AppPreferences
 import com.orgzly.android.query.user.InternalQueryParser
+import com.orgzly.android.repos.RepoFactory
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import java.io.StringWriter
 
-class FiletagsTest : OrgzlyTest() {
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
+class FiletagsTest {
+
+    private lateinit var context: Context
+    private lateinit var database: OrgzlyDatabase
+    private lateinit var dataRepository: DataRepository
+    private lateinit var testUtils: TestUtils
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+        AppPreferences.setToDefaults(context)
+
+        database = OrgzlyDatabase.forMemory(context)
+        val dbRepoBookRepository = DbRepoBookRepository(database)
+        val localStorage = LocalStorage(context)
+        val repoFactory = RepoFactory(context, dbRepoBookRepository)
+
+        dataRepository = DataRepository(
+            context, database, repoFactory, context.resources, localStorage
+        )
+
+        testUtils = TestUtils(dataRepository, dbRepoBookRepository)
+
+        dataRepository.clearDatabase()
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+
+    private fun exportBook(bookName: String): String {
+        val bookView = dataRepository.getBookView(bookName)!!
+        val sw = StringWriter()
+        NotesOrgExporter(dataRepository).exportBook(bookView.book, sw)
+        return sw.toString()
+    }
 
     @Test
     fun filetagsInheritance() {
@@ -83,8 +136,7 @@ class FiletagsTest : OrgzlyTest() {
         assertFalse(tags.contains("project"))
 
         // Filetags preserved on export
-        val bookView = dataRepository.getBookView("book-01")!!
-        val exported = exportBook(bookView)
+        val exported = exportBook("book-01")
         assertTrue(exported.contains("#+FILETAGS:"))
     }
 }
